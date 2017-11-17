b68d4da54cb3c88092feacdbc8211c48eaef7e0a
Cole Challand <ctchalland@gmail.com>
2017-11-16 18:12:33 -0600

    Made app RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get "/all_products_info" => "products#all_products_info_method"
-    get "/skates" => "products#skates_method"
-    get "/shins" => "products#shins_method"
-    get "/pants" => "product#pants_method"
-    get "/sholders" => "products#sholders_method"
-    get "/elbows" => "products#elbows_method"
-    get "/gloves" => "products#gloves_method"
-
-    get "/product/:input_product" => "products#product_method"
     get "/products" => "products#index"
     get "/products/:id" => "products#show" 
   end
