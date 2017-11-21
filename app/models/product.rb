fbb1479a5b7e3cdee979bb3a92ed4d686f5c6272
Cole Challand <ctchalland@gmail.com>
2017-11-20 22:08:43 -0600

    Add different migrations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ApplicationRecord
+  def is_discounted
+    price <= 2
+  end
+
+  def tax
+    return (price * 0.09)
+  end
+
+  def total
+    return (price + tax)
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      image: image,
+      description: description,
+      discounted: is_discounted,
+      tax: tax,
+      total: total,
+      availability: availability,
+      created_at: created_at,
+      updated_at: updated_at  
+    }
+  end
 end
---
+++ db/migrate/20171121014813_changed_description_to_text.rb
@@ -0,0 +1,5 @@
+class ChangedDescriptionToText < ActiveRecord::Migration[5.1]
+  def change
+    change_column :products, :description, :text
+  end
+end
