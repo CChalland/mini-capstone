d708c9101400a04dda734af67fc6f51a58f7918b
Cole Challand <ctchalland@gmail.com>
2017-11-28 23:03:33 -0600

    Add user_id to Orders conroller

--- app/controllers/v1/orders_controller.rb
+++ app/controllers/v1/orders_controller.rb
@@ -1,9 +1,10 @@
 class V1::OrdersController < ApplicationController
-
+  
   def create
     order = Order.new(
+      user_id: current_user.id,
       product_id: params[:product_id],
-      quantity: params[:quantity]
+      quantity: params[:quantity],
     )
     if order.save
       render json: order.as_json
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,32 +3,15 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :product
 
-  # def user_id
-  #   user.id
-  # end
-
   def subtotal
-    product.price * quantity
+    return (product.price * quantity)
   end
 
   def tax
-    subtotal * 0.09
+    return (subtotal * 0.09)
   end
 
   def total
-    subtotal + tax
+    return (subtotal + tax)
   end
-
-  # def as_json
-  #   {
-  #     id: id,
-  #     product_id: product_id
-  #     quantity: quantity,
-  #     sub_total: sub_total,
-  #     tax: tax,
-  #     total: total,
-  #     updated_at: updated_at
-  #   }
-  # end
-
 end
